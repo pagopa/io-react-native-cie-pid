3e006f806c6edfbf33da60d58619c309c5be1d91
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-26 13:26:19 +0200

    feat: add ios cie sdk code

--- io-react-native-cie-pid.podspec
+++ io-react-native-cie-pid.podspec
@@ -15,6 +15,10 @@ Pod::Spec.new do |s|
   s.source       = { :git => "https://github.com/pagopa/io-react-native-cie-pid.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm,swift}"
+  s.public_header_files = "ios/*.h"
+  s.vendored_frameworks = "ios/iociesdkios.framework"
+
+  s.frameworks = "iociesdkios"
 
   # Use install_modules_dependencies helper to install the dependencies if React Native version >=0.71.0.
   # See https://github.com/facebook/react-native/blob/febf6b7f33fdb4904669f99d795eba4c0f95d7bf/scripts/cocoapods/new_architecture.rb#L79.
--- ios/IoReactNativeCiePid.swift
+++ ios/IoReactNativeCiePid.swift
@@ -1,8 +1,113 @@
-@objc(IoReactNativeCiePid)
-class IoReactNativeCiePid: NSObject {
+import iociesdkios
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+let eventChannel = "onEvent"
+let errorChannel = "onError"
+let successChannel = "onSuccess"
+
+@objc(IoReactNativeCiePid)
+@available(iOS 13.0, *)
+class IoReactNativeCiePid: NSObject {
+    
+    var attemptsLeft: Int = 0
+    var PIN: String?
+    var url: String?
+    
+    var cieSDK: CIEIDSdk?
+    
+    override init() {
+        super.init()
+        self.cieSDK = CIEIDSdk()
+        
+    }
+    
+    @objc func isNFCEnabled(_ callback: RCTResponseSenderBlock) {
+        callback([true])
+    }
+    
+    @objc func hasNFCFeature(_ callback: RCTResponseSenderBlock) {
+        let value = cieSDK?.hasNFCFeature() ?? false
+        callback([NSNumber(value: value)])
+    }
+    
+    @objc func setPin(_ pin: String) {
+        self.PIN = pin
+    }
+    
+    func getPin() -> String? {
+        return self.PIN
+    }
+    
+    @objc func setAuthenticationUrl(_ url: String) {
+        self.url = url
+    }
+    
+    @objc func setAlertMessage(_ key: String, withValue value: String) {
+        cieSDK?.setAlertMessage(key: key, value: value)
+    }
+    
+    func getAuthenticationUrl() -> String? {
+        return self.url
+    }
+    
+    func post(callback: @escaping (String?, String?) -> Void) {
+        DispatchQueue.global().async {
+            self.cieSDK?.post(url: self.url!, pin: self.PIN!) { error, response in
+                callback(error, response)
+            }
+        }
+    }
+    
+    @objc func start(_ callback: RCTResponseSenderBlock) {
+        post { error, response in
+            if error == nil {
+                self.sendEvent(channel: successChannel, eventValue: response)
+            } else {
+                self.sendEvent(channel: eventChannel, eventValue: error)
+            }
+        }
+        callback([])
+    }
+    
+    func sendEvent(channel: String, eventValue: String?) {
+        let attemptsLeft = NSNumber(value: self.cieSDK?.attemptsLeft ?? 0)
+        let body: [String: Any] = ["event": eventValue ?? "", "attemptsLeft": attemptsLeft]
+        // self.sendEvent(channel: channel, eventValue: body["event"] as? String )
+    }
+    
+    @objc func launchCieID(_ callback: RCTResponseSenderBlock) {
+        // TODO: Implement CIE ID
+        callback([])
+    }
+    
+    // The following methods are not available on iOS
+    // Implement them if needed for Android or other platforms
+    
+    @objc func startListeningNFC(_ callback: RCTResponseSenderBlock) {
+        callback([])
+    }
+    
+    @objc func stopListeningNFC(_ callback: RCTResponseSenderBlock) {
+        callback([])
+    }
+    
+    @objc func openNFCSettings(_ callback: RCTResponseSenderBlock) {
+        callback([])
+    }
+    
+    @objc func hasApiLevelSupport(_ callback: RCTResponseSenderBlock) {
+        callback([])
+    }
+    
+    /*@objc(multiply:withB:withResolver:withRejecter:)
+     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+     resolve(a*b)
+     } */
+    /* @objc(initSDK:withRejecter:)
+     func initSDK(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+     
+     resolve(true)
+     } else {
+     reject("0", "init sdk failed", nil)
+     }
+     } */
 }
